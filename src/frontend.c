21049e969d3fd4c99778e16811c1368eb25525be
Andrey Nakin <andrey.nakin@gmail.com>
2018-07-02 12:27:04 +0300

    work

--- src/frontend.c
+++ src/frontend.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 #include <CAENDigitizer.h>
 
-#define CHECK(ret, msg)	\
-	if (ret) {	\
-		printf("Error %d %s\n", (int) ret, msg);	\
+#define ERREXIT() \
 		if (handle) {	\
 			CAEN_DGTZ_CloseDigitizer(handle);	\
 		}	\
-		exit(1);	\
+		exit(1)
+
+#define CHECK(ret, msg)	\
+	if (ret) {	\
+		printf("Error %d %s\n", (int) ret, msg);	\
+		ERREXIT();	\
 	}
 
 int const linkNum = 0;
@@ -17,21 +20,29 @@ int const conetNode = 0;
 uint32_t const vmeBaseAddr = 0;
 
 int main(int argc, char* argv[]) {
-	CAEN_DGTZ_ErrorCode ret;
-	CAEN_DGTZ_BoardInfo_t boardInfo;
-	int handle;
+CAEN_DGTZ_ErrorCode ret;
+CAEN_DGTZ_BoardInfo_t boardInfo;
+int handle;
 
-	ret = CAEN_DGTZ_OpenDigitizer(CAEN_DGTZ_OpticalLink, linkNum, conetNode,
-			vmeBaseAddr, &handle);
-	CHECK(ret, "opening digitizer");
+ret = CAEN_DGTZ_OpenDigitizer(CAEN_DGTZ_OpticalLink, linkNum, conetNode,
+		vmeBaseAddr, &handle);
+CHECK(ret, "opening digitizer");
 
-	ret = CAEN_DGTZ_GetInfo(handle, &boardInfo);
-	CHECK(ret, "getting digitizer info");
+ret = CAEN_DGTZ_GetInfo(handle, &boardInfo);
+CHECK(ret, "getting digitizer info");
 
-	printf("\nConnected to CAEN Digitizer Model %s\n", boardInfo.ModelName);
-	printf("ROC FPGA Release is %s\n", boardInfo.ROC_FirmwareRel);
-	printf("AMC FPGA Release is %s\n", boardInfo.AMC_FirmwareRel);
+printf("\nConnected to CAEN Digitizer Model %s\n", boardInfo.ModelName);
+printf("ROC FPGA Release is %s\n", boardInfo.ROC_FirmwareRel);
+printf("AMC FPGA Release is %s\n", boardInfo.AMC_FirmwareRel);
 
-	CAEN_DGTZ_CloseDigitizer(handle);
-	return 0;
+if (boardInfo.Model != CAEN_DGTZ_V1720) {
+	printf("The device is not CAEN V1720\n");
+	ERREXIT();
+}
+
+ret = CAEN_DGTZ_Reset(handle);
+CHECK(ret, "resetting digitizer");
+
+CAEN_DGTZ_CloseDigitizer(handle);
+return 0;
 }
