cc03983b35aa617c2fdb07f07ad059c52d71daec
Andrey Nakin <andrey.nakin@gmail.com>
2018-11-12 13:25:17 +0300

    work

--- src/frontend.c
+++ src/frontend.c
@@ -20,29 +20,47 @@ int const conetNode = 0;
 uint32_t const vmeBaseAddr = 0;
 
 int main(int argc, char* argv[]) {
-CAEN_DGTZ_ErrorCode ret;
-CAEN_DGTZ_BoardInfo_t boardInfo;
-int handle;
+	CAEN_DGTZ_ErrorCode ret;
+	CAEN_DGTZ_BoardInfo_t boardInfo;
+	int handle;
 
-ret = CAEN_DGTZ_OpenDigitizer(CAEN_DGTZ_OpticalLink, linkNum, conetNode,
-		vmeBaseAddr, &handle);
-CHECK(ret, "opening digitizer");
+	ret = CAEN_DGTZ_OpenDigitizer(CAEN_DGTZ_OpticalLink, linkNum, conetNode,
+			vmeBaseAddr, &handle);
+	CHECK(ret, "opening digitizer");
 
-ret = CAEN_DGTZ_GetInfo(handle, &boardInfo);
-CHECK(ret, "getting digitizer info");
+	ret = CAEN_DGTZ_GetInfo(handle, &boardInfo);
+	CHECK(ret, "getting digitizer info");
 
-printf("\nConnected to CAEN Digitizer Model %s\n", boardInfo.ModelName);
-printf("ROC FPGA Release is %s\n", boardInfo.ROC_FirmwareRel);
-printf("AMC FPGA Release is %s\n", boardInfo.AMC_FirmwareRel);
+	printf("***************************\n");
+	printf("CAEN Digitizer Model: %s\n", boardInfo.ModelName);
+	printf("Serial No: %d\n", boardInfo.SerialNumber);
+	printf("Number of channels: %d\n", boardInfo.Channels);
+	printf("Form-factor: %s\n", boardInfo.FormFactor == CAEN_DGTZ_VME64_FORM_FACTOR ? "VME64" : boardInfo.FormFactor == CAEN_DGTZ_VME64X_FORM_FACTOR ? "VME64X" : "Other");
+	printf("Number of ADC bits: %d\n", boardInfo.ADC_NBits);
+	printf("ROC FPGA Release: %s\n", boardInfo.ROC_FirmwareRel);
+	printf("AMC FPGA Release: %s\n", boardInfo.AMC_FirmwareRel);
+	printf("PCB Revision No: %d\n", boardInfo.PCB_Revision);
+	printf("COMM Handle: %x\n", boardInfo.CommHandle);
+	printf("VME Handle: %x\n", boardInfo.VMEHandle);
+	printf("***************************\n");
 
-if (boardInfo.Model != CAEN_DGTZ_V1720) {
-	printf("The device is not CAEN V1720\n");
-	ERREXIT();
-}
+	if (boardInfo.FamilyCode != CAEN_DGTZ_XX720_FAMILY_CODE) {
+		printf("The device is not of CAEN x720 family\n");
+		ERREXIT();
+	}
 
-ret = CAEN_DGTZ_Reset(handle);
-CHECK(ret, "resetting digitizer");
+	if (boardInfo.Model != CAEN_DGTZ_V1720) {
+		printf("The device is not CAEN V1720\n");
+		ERREXIT();
+	}
 
-CAEN_DGTZ_CloseDigitizer(handle);
-return 0;
+	ret = CAEN_DGTZ_Reset(handle);
+	CHECK(ret, "resetting digitizer");
+
+	ret = CAEN_DGTZ_SetDPPAcquisitionMode(handle, CAEN_DGTZ_DPP_ACQ_MODE_Mixed,
+			CAEN_DGTZ_DPP_SAVE_PARAM_None);
+	CHECK(ret, "setting DPP acquisition mode");
+
+	CAEN_DGTZ_CloseDigitizer(handle);
+	return 0;
 }
