7ab267fd49db442fd4694a23a0ec3e5fd9c5ed5c
Andrey Nakin <andrey.nakin@gmail.com>
2018-11-20 14:09:07 +0300

    work

--- fe-v1720/src/fe-v1720.cxx
+++ fe-v1720/src/fe-v1720.cxx
@@ -45,11 +45,101 @@ INT read_event(char *pevent, INT off);
 
 /*-- Equipment list ------------------------------------------------*/
 
+EQUIPMENT equipment[] = {
+
+{ "Trigger", /* equipment name */
+{ 1, TRIGGER_ALL, /* event ID, trigger mask */
+"SYSTEM", /* event buffer */
+EQ_POLLED, /* equipment type */
+LAM_SOURCE(0, 0xFFFFFF), /* event source */
+"MIDAS", /* format */
+TRUE, /* enabled */
+RO_RUNNING, /* read only when running */
+
+500, /* poll for 500ms */
+0, /* stop run after this event limit */
+0, /* number of sub events */
+0, /* don't log history */
+"", "", "", }, read_event, /* readout routine */
+NULL, NULL,
+NULL, /* bank list */
+},
+
+{ "" } };
+
 #ifdef __cplusplus
 }
 #endif
 
-int main(int argc, char* argv[]) {
-	caen::Handle handle(0, 0, 0);
+/*-- Frontend Init -------------------------------------------------*/
+
+INT frontend_init() {
+	INT status = SUCCESS;
+
+	return status;
+}
+
+/*-- Frontend Exit -------------------------------------------------*/
+
+INT frontend_exit() {
+
+	return SUCCESS;
+}
+
+/*-- Begin of Run --------------------------------------------------*/
+
+INT begin_of_run(INT run_number, char *error) {
+	return SUCCESS;
+}
+
+/*-- End of Run ----------------------------------------------------*/
+INT end_of_run(INT run_number, char *error) {
+	return SUCCESS;
+}
+
+/*-- Pause Run -----------------------------------------------------*/
+INT pause_run(INT run_number, char *error) {
+	return SUCCESS;
+}
+
+/*-- Resume Run ----------------------------------------------------*/
+INT resume_run(INT run_number, char *error) {
+	return SUCCESS;
+}
+
+/*-- Frontend Loop -------------------------------------------------*/
+INT frontend_loop() {
+	return SUCCESS;
+}
+
+/*-- Trigger event routines ----------------------------------------*/
+extern "C" INT poll_event(INT source, INT count, BOOL test) {
+	return FALSE;
+}
+
+/*-- Interrupt configuration ---------------------------------------*/
+extern "C" INT interrupt_configure(INT cmd, INT source, PTYPE adr) {
+	switch (cmd) {
+	case CMD_INTERRUPT_ENABLE:
+		break;
+	case CMD_INTERRUPT_DISABLE:
+		break;
+	case CMD_INTERRUPT_ATTACH:
+		break;
+	case CMD_INTERRUPT_DETACH:
+		break;
+	}
+	return SUCCESS;
+}
+
+/*-- Read Event -------------------------------------------------*/
+INT read_event(char *pevent, INT off) {
 	return 0;
 }
+
+/*
+ int main(int argc, char* argv[]) {
+ caen::Handle handle(0, 0, 0);
+ return 0;
+ }
+ */
