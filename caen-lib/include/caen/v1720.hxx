ed5f7f171e30dec1b2902302fdd495f3855902ef
Andrey Nakin <andrey@ter-net.com>
2018-12-26 21:03:13 +0300

    work

--- caen-lib/include/caen/v1720.hxx
+++ caen-lib/include/caen/v1720.hxx
@@ -2,8 +2,6 @@
 #define	__CAEN_V1720_HXX__
 
 #include <cstdint>
-#include "noncopyable.hxx"
-#include "error-holder.hxx"
 
 namespace caen {
 
@@ -17,8 +15,6 @@ constexpr uint32_t REG_CHANNEL_CONFIG = 0x8000;
 constexpr uint32_t REG_POST_TRIGGER = 0x8114;
 
 constexpr uint32_t REG_BIT_TRIGGER_OVERLAP = 0x0001 << 1;
-
-constexpr unsigned TOTAL_MEMORY_SIZE = 10 * 1024 * 1024 * sizeof(uint16_t);
 
 }
 
