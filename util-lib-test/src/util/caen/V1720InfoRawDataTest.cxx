e45b0f0df2ade7fde4475afe82011303165c1689
Andrey Nakin <andrey@ter-net.com>
2019-04-04 13:24:27 +0300

    work

--- util-lib-test/src/util/caen/V1720InfoRawDataTest.cxx
+++ util-lib-test/src/util/caen/V1720InfoRawDataTest.cxx
@@ -5,7 +5,21 @@ namespace util {
 
 namespace caen {
 
-TEST(V1720InfoRawData, Generic) {
+TEST(V1720InfoRawData, TimeStamp) {
+
+	{
+		InfoBank bank;
+		bank.timeStamp = 0x12345678;
+		V1720InfoRawData const e(sizeof(bank), TID_DWORD, V1720InfoRawData::bankName(), &bank);
+		EXPECT_EQ(0x12345678, e.timeStamp());
+	}
+
+	{
+		InfoBank bank;
+		bank.timeStamp = 0xffffffff;
+		V1720InfoRawData const e(sizeof(bank), TID_DWORD, V1720InfoRawData::bankName(), &bank);
+		EXPECT_EQ(0xffffffff, e.timeStamp());
+	}
 
 }
 
--- util-lib-test/src/util/caen/V1724InfoRawDataTest.cxx
+++ util-lib-test/src/util/caen/V1724InfoRawDataTest.cxx
@@ -5,7 +5,21 @@ namespace util {
 
 namespace caen {
 
-TEST(V1724InfoRawData, Generic) {
+TEST(V1724InfoRawData, TimeStamp) {
+
+	{
+		InfoBank bank;
+		bank.timeStamp = 0x12345678;
+		V1724InfoRawData const e(sizeof(bank), TID_DWORD, V1724InfoRawData::bankName(), &bank);
+		EXPECT_EQ(0x12345678, e.timeStamp());
+	}
+
+	{
+		InfoBank bank;
+		bank.timeStamp = 0xffffffff;
+		V1724InfoRawData const e(sizeof(bank), TID_DWORD, V1724InfoRawData::bankName(), &bank);
+		EXPECT_EQ(0xffffffff, e.timeStamp());
+	}
 
 }
 
