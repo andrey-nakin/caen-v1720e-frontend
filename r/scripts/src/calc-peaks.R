1918039a09d777d28885294199f21977ae0545f0
Andrey Nakin <andrey@ter-net.com>
2019-05-30 13:17:52 +0300

    work

---
+++ r/scripts/src/calc-peaks.R
@@ -0,0 +1,294 @@
+#!/usr/bin/Rscript
+# Arguments: <master trigger> <trigger> <destination file> <MID file1 [...]> 
+
+library(optparse)
+library(gneis.daq)
+
+########################################################
+# Global variables
+########################################################
+
+my.front.lengths <- rep(4, times = 8)
+my.tail.lengths <- rep(10, times = 8)
+
+my.last.master.run <- 0
+my.last.master.eventCounter <- 0
+my.last.master.timeStamp <- 0
+my.last.master.trigger.position <- 0
+my.last.master.peak.position <- 0
+
+########################################################
+# Functions
+########################################################
+
+my.print.init.info <- function(e) {
+  cat(
+    file = my.dest, 
+    sep = "\t",
+    "DATETIME",
+    "RUN",
+    "EC",
+    "TS",
+    "TRG",
+    "PP",
+    "EPP",
+    "PA",
+    "EPA",
+    "FI",
+    "TI",
+    "\n"
+  )
+}
+
+my.print.channel <- function(my.info, wf, trg.ch, trg.pos) {
+  my.pulse <- get.pulse(
+    wf,
+    front.len = my.opt$options$front,
+    tail.len = my.opt$options$tail,
+    n.skip = my.opt$options$skip,
+    need.integration = TRUE
+  )
+  
+  if (is.null(my.pulse)) {
+    return(0)
+  }
+
+  if (my.last.master.run == my.info$Run && !is.na(my.last.master.peak.position)) {
+    my.pos <- (my.pulse$x - (trg.pos + 1)) - 
+      (my.last.master.peak.position - (my.last.master.trigger.position + 1))  +
+      timestamp.diff(my.info$DeviceTimeStamp, my.last.master.timeStamp) * my.info$TicksPerSample
+    my.pos.err <- my.pulse$x.err
+    if (my.last.master.eventCounter != my.info$EventCounter) {
+      my.pos.err <- my.pos.err + 1
+    }
+  } else {
+    my.pos <- my.pulse$x - (trg.pos + 1)
+    my.pos.err <- my.pulse$x.err
+  }
+  
+  if (my.pos > 9999) {
+    my.pos <- round(my.pos)
+  } else {
+    my.pos <- format(my.pos, digits = my.opt$options$precision)
+  }
+  
+  if (my.write.header) {
+    my.print.init.info(e)
+    my.write.header <<- FALSE
+  }
+  
+  cat(
+    file = my.dest, 
+    sep = "\t",
+    my.info$TimeStamp,
+    my.info$Run,
+    my.info$EventCounter,
+    my.info$DeviceTimeStamp,
+    trg.ch,
+    my.pos,
+    format(my.pos.err, digits = my.opt$options$precision),
+    format(my.pulse$amp, digits = my.opt$options$precision),
+    format(my.pulse$amp.err, digits = my.opt$options$precision),
+    format(my.pulse$front.int, digits = my.opt$options$precision),
+    format(my.pulse$tail.int, digits = my.opt$options$precision),
+    "\n"
+  )
+  
+  return(1)
+}
+
+my.event.collector <- function(a, e) {
+  my.info <- e$eventInfo
+  my.trg <- e$triggers
+
+  if (!is.na(my.trigger.col) && !(my.trg[[my.trigger.col]][1] > 0)) {
+    return(a)
+  }
+    
+  if (!is.na(my.master.trigger.col) && my.trg[[my.master.trigger.col]][1] > 0) {
+    my.last.master.run <<- my.info$Run
+    my.last.master.eventCounter <<- my.info$EventCounter
+    my.last.master.timeStamp <<- my.info$DeviceTimeStamp
+    my.last.master.trigger.position <<- my.trg[[my.master.trigger.col]][4]
+    my.mc <- get.pulse(
+      e$waveforms[[my.master.trigger.col]],
+      front.len = my.opt$options$masterfront,
+      tail.len = my.opt$options$mastertail,
+      n.skip = my.opt$options$masterskip
+    )
+    if (!is.null(my.mc)) {
+      my.last.master.peak.position <<- my.mc$x
+    } else {
+      my.last.master.peak.position <<- NA
+    }
+  }
+
+  my.wf <- e$waveforms[[my.channel.col]]
+  if (is.null(my.wf)) {
+    return(a)
+  }
+  
+  my.trg.ch <- 0
+  my.trg.pos <- 0
+  for (col in colnames(my.trg)) {
+    if (my.trg[[col]][1]) {
+      my.trg.pos <- my.trg[[col]][4]
+      break;
+    }
+    my.trg.ch <- my.trg.ch + 1  
+  }
+
+  if (a %% 1000 == 0 && my.opt$options$verbose) {
+    cat(e$eventInfo$Run, e$eventInfo$EventCounter, "\n")
+  }
+  
+  my.res <- my.print.channel(my.info, my.wf, my.trg.ch, my.trg.pos)
+  
+  return(a + my.res)
+}
+
+my.make.filename <- function(opt) {
+  res <- paste(opt$args[1], "/peaks.ch", opt$options$channel, sep = "")
+  
+  if (!is.na(opt$options$master)) {
+    res <- paste(res, ".mst", opt$options$master, sep = "")
+  }
+  
+  if (!is.na(opt$options$trigger)) {
+    res <- paste(res, ".trg", opt$options$trigger, sep = "")
+  }
+  
+  if (length(my.opt$args) == 2) {
+    runname <- strsplit(basename(my.opt$args[2]), "\\.")[[1]][1]
+    res <- paste(res, runname, sep = ".")
+  }
+  
+  res <- paste(res, "txt", sep = ".")
+  
+  return (res)
+}
+
+my.make.column.name <- function(channel) {
+  if (is.na(channel)) {
+    return (NA)
+  } else {
+    return (paste("CH", channel, sep = ""))
+  }
+}
+
+my.stop.func <- function(a) {
+  return(!is.na(my.opt$options$number) && my.opt$options$number > 0 && !(a < my.opt$options$number))
+}
+
+########################################################
+# Command line parsing
+########################################################
+
+my.option.list <- list( 
+  make_option(
+    c("-c", "--channel"),
+    type = "integer",
+    default = NA, 
+    help = "Channel #"
+  ),
+  make_option(
+    c("", "--front"),
+    type = "integer",
+    default = 10, 
+    help = "Number of samples before the peak (default %default)"
+  ),
+  make_option(
+    c("", "--tail"),
+    type = "integer",
+    default = 10, 
+    help = "Number of samples before the peak (default %default)"
+  ),
+  make_option(
+    c("-m", "--master"),
+    type = "integer",
+    default = NA, 
+    help = "Trigger channel # used for time calcs."
+  ),
+  make_option(
+    c("", "--masterfront"),
+    type = "integer",
+    default = 10, 
+    help = "Number of samples before the peak in master channel (default %default)"
+  ),
+  make_option(
+    c("", "--mastertail"),
+    type = "integer",
+    default = 10, 
+    help = "Number of samples before the peak in master channel (default %default)"
+  ),
+  make_option(
+    c("", "--masterskip"),
+    type = "integer",
+    default = 50, 
+    help = "Number of samples after the peak to skip in master channel (default %default)"
+  ),
+  make_option(
+    c("-n", "--number"),
+    type = "integer",
+    default = NA, 
+    help = "Max number of waveforms to process"
+  ),
+  make_option(
+    c("-p", "--precision"),
+    type = "integer",
+    default = 4, 
+    help = "Number precision"
+  ),
+  make_option(
+    c("", "--skip"),
+    type = "integer",
+    default = 50, 
+    help = "Number of samples after the tail to exclude from mean level calcs"
+  ),
+  make_option(
+    c("-t", "--trigger"),
+    type = "integer",
+    default = NA, 
+    help = "Trigger channel #"
+  ),
+  make_option(
+    c("-v", "--verbose"),
+    action="store_true",
+    default = FALSE, 
+    help = "Verbose mode"
+  )
+)
+
+my.opt <- parse_args(
+  OptionParser(
+    option_list = my.option.list,
+    usage = "%prog [options] <destination directory> <MID file 1> [<MID file 2> ...]"
+  ), 
+  positional_arguments = c(2, Inf)
+)
+
+if (is.na(my.opt$options$channel)) {
+  stop("ERROR: channel is not specified")
+}
+
+my.midas.files <- tail(my.opt$args, n = -1)
+my.channel.col <- my.make.column.name(my.opt$options$channel)
+my.master.trigger.col <- my.make.column.name(my.opt$options$master)
+my.trigger.col <- my.make.column.name(my.opt$options$trigger)
+
+########################################################
+# Processing
+########################################################
+
+my.dest <- file(my.make.filename(my.opt))
+open(my.dest, "w")
+my.write.header <- TRUE
+
+my.res <- read.events.from.gdconvert(
+  file.names = my.midas.files,
+  merging.func = my.event.collector,
+  stop.func = my.stop.func,
+  init.value = 0
+)
+
+close(my.dest)
--- r/scripts/src/prefission/peak-distr.R
+++ r/scripts/src/prefission/peak-distr.R
@@ -1,102 +1,283 @@
 #!/usr/bin/Rscript
-# Arguments: [<options>] <peaks file> <dest file> <channel>
 
-library(gneis.daq)
+library(optparse)
+library(ggplot2)
+library(stats)
+library(plotly)
+library(tidyverse)
+library(htmlwidgets)
 
-my.master.channel <- 0  # number of master trigger channel
-my.dist.channel <- 0  # number of channel of interest
-my.timestamp.module <- 2^31   # module of time stamp values
+########################################################
+# Functions
+########################################################
 
-#####################################################
-# Command line parsing
-#####################################################
-
-my.cmd.args <- commandArgs(trailingOnly=TRUE)
-my.file.peaks <- my.cmd.args[1]
-my.file.dest <- my.cmd.args[2]
-if (length(my.cmd.args) >= 3) {
-  my.dist.channel <- as.numeric(my.cmd.args[3])
+my.make.txt.filename.mask <- function(opt, notrigger = FALSE) {
+  res <- paste("peaks.ch", opt$options$channel, sep = "")
+  
+  if (!is.na(opt$options$master)) {
+    res <- paste(res, ".mst", opt$options$master, sep = "")
+  }
+  
+  if (!is.na(opt$options$trigger) && !notrigger) {
+    res <- paste(res, ".trg", opt$options$trigger, sep = "")
+  }
+  
+  res <- paste(res, "run[0-9]+", "txt", sep = ".")
+  return (res)
 }
 
-#####################################################
-# File processing
-#####################################################
+my.make.dest.filename <- function(opt, dir, ext, suffix = NULL) {
+  res <- paste(dir, "/peaks.ch", opt$options$channel, sep = "")
+  
+  if (!is.na(opt$options$master)) {
+    res <- paste(res, ".mst", opt$options$master, sep = "")
+  }
+  
+  if (!is.na(opt$options$trigger)) {
+    res <- paste(res, ".trg", opt$options$trigger, sep = "")
+  }
+  
+  res <- paste(res, "checked", paste("amp", opt$options$step, sep = ""), "dist", sep = ".")
+  if (!is.null(suffix)) {
+    res <- paste(res, suffix, sep = ".")
+  }
+  res <- paste(res, ext, sep = ".")
+  
+  return (res)
+}
 
-my.peaks <- file(my.file.peaks)
-open(my.peaks, "r")
+my.make.plot.filename <- function(opt, suffix = NULL) {
+  return(
+    my.make.dest.filename(opt, opt$args[3], "pdf", suffix = suffix)
+  )
+}
 
-my.dest <- file(my.file.dest)
-open(my.dest, "w")
+my.make.html.filename <- function(opt, suffix = NULL) {
+  return(
+    my.make.dest.filename(opt, opt$args[4], "html", suffix = suffix)
+  )
+}
 
-cat(
-  file = my.dest, 
-  sep = "\t",
-  "RUN",
-  "EC",
-  "CH",
-  "TRG",
-  "PP",
-  "PA",
-  "FI",
-  "TI",
-  "\n"
+my.make.result.filename <- function(opt, suffix = NULL) {
+  return(
+    my.make.dest.filename(opt, opt$args[2], "txt", suffix = suffix)
+  )
+}
+
+my.extract.run <- function(fn) {
+  return(sub(".*\\.(run[0-9]+)\\..*", "\\1", basename(fn), perl=TRUE))
+}
+
+my.make.channel.fn <- function(fn, channel) {
+  my.res <- sub("\\.ch([0-9])\\.", paste(".ch", channel, ".", sep = ""), fn, perl = T)
+  return(my.res)
+}
+
+my.process.file <- function(fn, accum, breaks) {
+  
+  my.df1 <- read.table(my.make.channel.fn(fn, 1), header = T)
+  my.df2 <- read.table(my.make.channel.fn(fn, 2), header = T)
+  my.df3 <- read.table(my.make.channel.fn(fn, 3), header = T)
+  my.df4 <- read.table(my.make.channel.fn(fn, 4), header = T)
+  my.df5 <- read.table(my.make.channel.fn(fn, 5), header = T)
+
+  my.ec1 <- my.df1$EC[which(abs(my.df1$PA) >= 75)]
+  my.ec2 <- my.df2$EC[which(abs(my.df2$PA) >= 65)]
+  my.ec3 <- my.df3$EC[which(abs(my.df3$PA) >= 35)]
+  my.ec4 <- my.df4$EC[which(abs(my.df4$PA) >= 35)]
+  my.ec <- unique(append(my.ec1, append(my.ec2, append(my.ec3, my.ec4))))
+  my.ec <- sort(my.ec)
+  cat("filtering...", length(my.df5$EC), length(my.ec), "\n")
+  my.x <- abs(my.df5$PA[which(my.df5$EC %in% my.ec)])
+  cat("filtering done", length(my.x), "\n")
+  my.x <- my.x[which(my.x >= my.opt$options$min & my.x < my.opt$options$max)]
+  
+  my.total <<- my.total + length(my.x)
+  my.h <- hist(my.x, breaks = breaks, plot = F)
+  
+  if (is.null(accum)) {
+    return(my.h$counts)
+  }
+  
+  return(accum + my.h$counts)
+  
+}
+
+my.plot.title <- function(main) {
+  my.res <- paste(main, paste("Channel #", my.opt$options$channel, sep = ""), sep = ", ")
+  if (!is.na(my.opt$options$master)) {
+    my.res <- paste(my.res, paste("Master #", my.opt$options$master, sep = ""), sep = ", ")
+  }
+  if (!is.na(my.opt$options$trigger)) {
+    my.res <- paste(my.res, paste("Trigger #", my.opt$options$trigger, sep = ""), sep = ", ")
+  }
+  return(my.res)
+}
+
+my.plot <- function() {
+  my.data <- data.frame(
+    x = my.x,
+    y = my.accum
+  )
+  my.ci <- stats::binom.test(x = max(my.accum), n = sum(my.accum), p = 1/length(my.accum), conf.level = 0.95)$conf.int
+  my.err.min <- (my.ci[2] - my.ci[1]) / 2
+  
+  pdf(my.make.plot.filename(my.opt))
+  
+  my.p <- ggplot(
+    data = my.data, 
+    aes(x = x, y = y)
+  ) + 
+    geom_line() +
+    ggtitle(
+      label = my.plot.title("Amplitude Distribution"),
+      subtitle = paste(
+        paste("Total", format(my.total, digits = 2), sep = ": "),
+        #      paste("MinE", format(my.err.min, digits = 2), sep = ": "),
+        sep = ", "
+      )
+    ) +
+    scale_x_continuous(name = "Absolute Amplitude, channels") + 
+    scale_y_continuous(name = "Count") +
+    theme_light()
+  
+  plot(my.p)
+  
+  htmlwidgets::saveWidget(ggplotly(my.p), my.make.html.filename(my.opt))
+  
+}
+
+my.plot.derivative <- function() {
+  my.data <- data.frame(
+    x = my.der.x,
+    y = my.der.y
+  )
+  
+  pdf(my.make.plot.filename(my.opt, suffix = "der"))
+  
+  my.p <- ggplot(
+    data = my.data, 
+    aes(x = x, y = y)
+  ) + 
+    geom_line() +
+    ggtitle(
+      label = my.plot.title("Amplitude Distribution Derivative")
+    ) +
+    scale_x_continuous(name = "Absolute Amplitude, channels") + 
+    scale_y_continuous(name = "Derivative") +
+    theme_light()
+  
+  plot(my.p)
+  
+  htmlwidgets::saveWidget(ggplotly(my.p), my.make.html.filename(my.opt, suffix = "der"))
+  
+}
+
+my.write.result <- function() {
+  my.df <- data.frame(
+    AMP = my.x,
+    COUNT = my.accum
+  )
+  write.table(
+    my.df, 
+    file = my.make.result.filename(my.opt),
+    sep = "\t",
+    row.names = F
+  )
+  
+  my.df <- data.frame(
+    AMP = my.der.x,
+    DERIVATIVE = my.der.y
+  )
+  write.table(
+    my.df, 
+    file = my.make.result.filename(my.opt, suffix = "der"),
+    sep = "\t",
+    row.names = F
+  )
+}
+
+########################################################
+# Command line parsing
+########################################################
+
+my.option.list <- list( 
+  make_option(
+    c("-c", "--channel"),
+    type = "integer",
+    default = NA, 
+    help = "Channel #"
+  ),
+  make_option(
+    c("-m", "--master"),
+    type = "integer",
+    default = NA, 
+    help = "Trigger channel # used for time calcs."
+  ),
+  make_option(
+    c("-t", "--trigger"),
+    type = "integer",
+    default = NA, 
+    help = "Trigger channel #"
+  ),
+  make_option(
+    c("", "--min"),
+    type = "double",
+    default = 0, 
+    help = "Min value"
+  ),
+  make_option(
+    c("", "--max"),
+    type = "double",
+    default = NA, 
+    help = "Max value"
+  ),
+  make_option(
+    c("", "--step"),
+    type = "double",
+    default = 1, 
+    help = "Distribution step"
+  ),
+  make_option(
+    c("-n", "--number"),
+    type = "integer",
+    default = NA, 
+    help = "Max # of samples"
+  )
 )
 
-my.last.master.run <- 0
-my.last.master.eventCounter <- 0
-my.last.master.timeStamp <- 0
-my.last.master.trigger.position <- 0
-my.last.master.peak.position <- 0
+my.opt <- parse_args(
+  OptionParser(
+    option_list = my.option.list,
+    usage = "%prog [options] <src file dir> <txt file dir> <plot file dir> <html file dir>"
+  ), 
+  positional_arguments = c(4, 4)
+)
 
-while (length(line <- readLines(my.peaks, n=1)) > 0) {
-  my.s <- strsplit(line, split = "\t")[[1]]
-  
-  my.channel <- as.numeric(my.s[4])
-  if (is.na(my.channel)) {
-    next
-  }
+########################################################
+# Processing
+########################################################
 
-  if (my.channel == my.master.channel) {
-    my.is.trigger <- as.numeric(my.s[5])
-    if (my.is.trigger > 0) {
-      my.last.master.run <- as.numeric(my.s[1])
-      my.last.master.eventCounter <- as.numeric(my.s[2])
-      my.last.master.timeStamp <- as.numeric(my.s[3])
-      my.last.master.trigger.position <-as.numeric(my.s[6]) 
-      my.last.master.peak.position <- as.numeric(my.s[7])
-    }
+my.total <- 0
+my.accum <- NULL
+my.breaks <- seq(from = my.opt$options$min, to = my.opt$options$max, by = my.opt$options$step)
+my.x <- tail(my.breaks, n = -1) - my.opt$options$step / 2
+
+my.files <- list.files(path = my.opt$args[1], pattern = my.make.txt.filename.mask(my.opt), full.names = T)
+if (length(my.files) == 0) {
+  my.files <- list.files(path = my.opt$args[1], pattern = my.make.txt.filename.mask(my.opt, notrigger = T), full.names = T)
+}
+for (my.fn in my.files) {
+  my.accum <- my.process.file(my.fn, my.accum, my.breaks)
+  if (!is.na(my.opt$options$number) && my.total >= my.opt$options$number) {
+    break
   }
-  
-  if (my.channel == my.dist.channel) {
-    my.run <- as.numeric(my.s[1])
-    my.eventCounter <- as.numeric(my.s[2])
-    my.is.trigger <- as.numeric(my.s[5])
-    
-    if (my.run == my.last.master.run && my.eventCounter >= my.last.master.eventCounter && my.is.trigger > 0) {
-      my.timeStamp <- as.numeric(my.s[3])
-      my.trigger.position <-as.numeric(my.s[6])
-      my.peak.position <- as.numeric(my.s[7])
-      my.timeStamp.diff <- (my.timeStamp - my.last.master.timeStamp) %% my.timestamp.module
-      my.time <- my.peak.position - my.trigger.position - 
-        (my.last.master.peak.position - my.last.master.trigger.position) + 
-        my.timeStamp.diff
-      
-      cat(
-        file = my.dest, 
-        sep = "\t",
-        my.run, 
-        my.eventCounter, 
-        my.dist.channel,
-        my.is.trigger,
-        my.time, 
-        my.s[8], 
-        my.s[9], 
-        my.s[10], 
-        "\n")
-    }
-  }
-  
 }
 
-close(my.peaks)
-close(my.dest)
+my.der.x <- tail(my.x, n = -1) - my.opt$options$step / 2
+my.der.y <- diff(my.accum) / my.opt$options$step
+
+my.write.result()
+
+my.plot()
+my.plot.derivative()
